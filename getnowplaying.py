636a2af3d5a0e7aa6c461bd56269c5b5f4985b49
Nick Presta <nick@nickpresta.ca>
2011-07-09 16:44:45 -0400

    Added support for both types of profiles (custom URL and default 17 digit ID). Added a README.

--- getnowplaying.py
+++ getnowplaying.py
@@ -13,12 +13,21 @@ from xml.etree import ElementTree
 class SteamUserDataFetcher(object):
     """Fetches user data from Steam for a specific user"""
 
-    PROFILE_URI = 'http://steamcommunity.com/id/%s?xml=1'
+    PROFILE_CUSTOM_URI = 'http://steamcommunity.com/id/%s?xml=1'
+    PROFILE_DEFAULT_URI = 'http://steamcommunity.com/profiles/%s?xml=1'
+
+    def _choose_uri(self, user_id):
+        """If a user ID is a 17 digit integer string, use the default URI
+           otherwise, use the custom URI"""
+        mapping = {True: SteamUserDataFetcher.PROFILE_DEFAULT_URI,
+                   False: SteamUserDataFetcher.PROFILE_CUSTOM_URI}
+        self._profile_uri = mapping[len(user_id) == 17 and user_id.isdigit()] % user_id
+        return self._profile_uri
 
     def _fetch_user_data(self, user_id):
         """Fetches the raw XML from the user's profile page
             and returns an XML document object"""
-        page = urllib2.urlopen(SteamUserDataFetcher.PROFILE_URI % user_id)
+        page = urllib2.urlopen(self._choose_uri(user_id))
         data = page.read()
         page.close()
         return ElementTree.fromstring(data)
@@ -30,6 +39,9 @@ class SteamUserDataFetcher(object):
                 # Player details
                 'steamID64': document.findtext('steamID64'),
                 'steamID': document.findtext('steamID'),
+                'customURL': document.findtext('customURL'),
+                'privacyState': document.findtext('privacyState'),
+                'visibilityState': document.findtext('visibilityState'),
                 'onlineState': document.findtext('onlineState'),
                 # Game details
                 'inGameServerIP': document.findtext('inGameServerIP'),
@@ -43,6 +55,8 @@ class SteamUserDataFetcher(object):
         except AttributeError:
             # Usually means user doesn't exist, otherwise the stateMessage element would exist
             out_dict['stateMessage'] = None
+
+        out_dict['profileURL'] = self._profile_uri.replace('?xml=1', '')
 
         return out_dict
 
